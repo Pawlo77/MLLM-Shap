8d60e9b126967cc97a305b3916df19e18c8e5ba5
Paweł Pozorski <pozorski.paul@gmail.com>
2025-10-05 23:10:46 +0200

    refactor(experiments-constants): Hugging face configs names included in dataset DatasetConfig

--- experiments/constants.py
+++ experiments/constants.py
@@ -18,11 +18,34 @@ class DatasetConfig(BaseModel):
     data_dir: Path
     cache_dir: Path
     revision: str
+    configs: dict[str, str]
 
 
-VOICE_BENCH_CONFIG = DatasetConfig(
+VOICE_BENCH__CONFIG = DatasetConfig(
     dataset_name="hlt-lab/voicebench",
     data_dir=ensure_dir(DATA_DIR / "voicebench"),
     cache_dir=ensure_dir(DATA_DIR / "voicebench" / ".cache"),
     revision="b02edcef1330480be3a11bd6f7434ac32f05ad08",
+    configs={
+        "AdvBench": "advbench",
+        "AlpacaEval": "alpacaeval",
+        "AlpacaEval-Full": "alpacaeval_full",
+        "AlpacaEval-Speaker": "alpacaeval_speaker",
+        "BBH": "bbh",
+        "CommonEval": "commoneval",
+        "IFEval": "ifeval",
+        "MMSU": "mmsu",
+        "MT-Bench": "mtbench",
+        "OpenBookQA": "openbookqa",
+        "SD-QA": "sd-qa",
+        "WildVoice": "wildvoice",
+    },
+)
+
+INFINITY_INSTRUCT__CONFIG = DatasetConfig(
+    dataset_name="BAAI/Infinity-Instruct",
+    data_dir=ensure_dir(DATA_DIR / "infinity_instruct"),
+    cache_dir=ensure_dir(DATA_DIR / "infinity_instruct" / ".cache"),
+    revision="main",
+    configs={"660k": "0625"},
 )
