a224960760c2818b2acfa5b63e51431c4b06d00f
Paweł Pozorski <pozorski.paul@gmail.com>
2025-11-08 20:10:08 +0100

    chore: Prepare interface for hierarchical explainer, add new simmi larity function and examplatory graph code used in paper

--- mllm_shap/src/mllm_shap/shap/compact.py
+++ mllm_shap/src/mllm_shap/shap/compact.py
@@ -4,86 +4,30 @@ from logging import Logger
 from time import time
 from typing import Any
 
-from pydantic import BaseModel, ConfigDict
-from torch import Tensor
 
 from ..connectors.base.chat import BaseMllmChat
-from ..connectors.base.model import BaseMllmModel
-from ..connectors.base.model_response import ModelResponse
 from ..utils.logger import get_logger
-from .base.explainer import BaseShapExplainer
+from .base.explainer import BaseShapExplainer, BaseExplainer
+from .explainer_result import ExplainerResult
 from .precise import PreciseShapExplainer
 
 logger: Logger = get_logger(__name__)
 
 
 # pylint: disable=too-few-public-methods
-class _ExplainerConfig(BaseModel):
-    """
-    Configuration model for Explainer.
-    Used just for validation and type checking.
-    """
-
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    shap_explainer: BaseShapExplainer
-    model: BaseMllmModel
-
-
-# pylint: disable=too-few-public-methods
-class ExplainerResult(BaseModel):
-    """Result model for Explainer."""
-
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    """Configuration for pydantic model."""
-
-    full_chat: BaseMllmChat
-    """The full chat instance after generation (entire conversation). It will be set with SHAP values and cache."""
-
-    source_chat: BaseMllmChat
-    """Chat to get explained (without base response)."""
-
-    history: list[tuple[Tensor, int, BaseMllmChat | None, ModelResponse]] | None
-    """
-    The history of chats and masks used during explanation
-    (if applicable, that is if explainer was called with `verbose=True`).
-    Each entry is a tuple of  (mask, mask_hash, masked_chat, model_response)
-    If cache was used, masked_chat will be None.).
-    """
-
-
-# pylint: disable=too-few-public-methods
-class Explainer:
-    """SHAP explainer for audio models."""
-
-    shap_explainer: BaseShapExplainer
-    """The SHAP explainer instance."""
-
-    model: BaseMllmModel
-    """The model connector instance."""
+class Explainer(BaseExplainer):
+    """Convenience client class for SHAP explanation."""
 
     def __init__(
         self,
-        model: BaseMllmModel,
         shap_explainer: BaseShapExplainer | None = None,
+        **kwargs: Any,
     ) -> None:
-        """
-        Initialize the explainer.
-
-        Args:
-            model: The model connector instance.
-            shap_explainer: The SHAP explainer instance.
-        """
-        # validation
-        __config = _ExplainerConfig(
+        super().__init__(
             shap_explainer=shap_explainer or PreciseShapExplainer(),
-            model=model,
+            **kwargs,
         )
 
-        self.shap_explainer = __config.shap_explainer
-        self.model = __config.model
-
-    # pylint: disable=magic-value-comparison
     def __call__(
         self,
         *_: Any,
@@ -91,25 +35,12 @@ class Explainer:
         generation_kwargs: dict[str, Any] | None = None,
         **explanation_kwargs: Any,
     ) -> ExplainerResult:
-        """
-        Call the explainer - generate full response from :attr:`chat`
-        using :attr:`model`, and then explain it using :attr:`shap_explainer`.
-
-        Args:
-            chat: The chat instance.
-            generation_kwargs: The generation kwargs for the model.generate method.
-            explanation_kwargs: The explanation kwargs for the SHAP explainer. Shoul not contain
-                duplicate keys with generation_kwargs.
-        Returns:
-            The ExplainerResult instance.
-        Raises:
-            ValueError: If generation_kwargs or explanation_kwargs contain invalid keys.
-        """
         generation_kwargs = generation_kwargs or {}
-        if "chat" in generation_kwargs or "keep_history" in generation_kwargs:
-            raise ValueError("generation_kwargs should not contain 'chat' or 'keep_history' keys.")
-        if "chat" in explanation_kwargs or "base_chat" in explanation_kwargs or "model" in explanation_kwargs:
-            raise ValueError("explanation_kwargs should not contain 'chat', 'base_chat' or 'model' keys.")
+        super().__call__(
+            chat=chat,
+            generation_kwargs=generation_kwargs,
+            **explanation_kwargs,
+        )
 
         t0 = time()
         logger.info("Generating full response from the model...")
