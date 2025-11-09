f539a45ffe140f208db6e7c77b7eed0f2c30e8dd
mvishiu11 <jakubmuszynski2103@gmail.com>
2025-11-09 16:03:53 +0100

    fix: set tokenizer before the super init

--- mllm_shap/src/mllm_shap/connectors/__init__.py
+++ mllm_shap/src/mllm_shap/connectors/__init__.py
@@ -2,7 +2,7 @@
 
 from .config import ModelConfig
 from .liquid_audio import LiquidAudio, LiquidAudioChat
-from .transfomers_text import TransformersCausalText, TransformersTextChat
+from .transformers_text import TransformersCausalText, TransformersTextChat
 
 __all__ = ["LiquidAudioChat", "LiquidAudio",
            "TransformersTextChat", "TransformersCausalText",
