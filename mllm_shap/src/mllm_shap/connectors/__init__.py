35a469e1c17a3047c02a1ba2fdbc3507277c8a27
mvishiu11 <jakubmuszynski2103@gmail.com>
2025-11-09 15:42:38 +0100

    chore: change directory naming

--- mllm_shap/src/mllm_shap/connectors/__init__.py
+++ mllm_shap/src/mllm_shap/connectors/__init__.py
@@ -2,7 +2,7 @@
 
 from .config import ModelConfig
 from .liquid_audio import LiquidAudio, LiquidAudioChat
-from .text import TransformersCausalText, TransformersTextChat
+from .transfomers_text import TransformersCausalText, TransformersTextChat
 
 __all__ = ["LiquidAudioChat", "LiquidAudio",
            "TransformersTextChat", "TransformersCausalText",
